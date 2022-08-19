e831d4deac7df303eba3dd55dd19e1ebe9b3f494
Nivethitha-27 <nivethitharaj27@gmail.com>
2022-08-19 13:10:41 +0530

    first commit

--- Routes/trainRoute.js
+++ Routes/trainRoute.js
@@ -90,7 +90,7 @@ route.get("/find/:key", async (req, res) => {
   console.log(req.params.key);
   try {
     const data = await train.find({
-      "$and": [
+      $and: [
         { from: { $regex: req.params.key } },
         { to: { $regex: req.params.key } }
       ]
